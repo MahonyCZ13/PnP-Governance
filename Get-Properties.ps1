48c0a3bee33ca281400a19bf8f440c7eea5ca24d
MahonyCZ13 <maronek.petr@outlook.com>
2024-03-11 18:13:40 +0100

    saving more data to SQL DB

--- Get-Properties.ps1
+++ Get-Properties.ps1
@@ -6,11 +6,10 @@ $connectionParameters = @{
     Tenant = $settings.tenant
     TenantAdminUrl = $settings.adminUrl
 }
-$url = $settings.SiteUrl
 
 try {
-    $conn = Connect-PnPOnline -Url $url @connectionParameters -ReturnConnection
-    $currentSite = Get-PnPTenantSite -Connection $conn -Detailed -Identity $url
+    $conn = Connect-PnPOnline -Url $settings.rootUrl @connectionParameters -ReturnConnection
+    $allSites = Get-PnPTenantSite -Connection $conn -Detailed
     
 }
 catch {
@@ -19,28 +18,6 @@ catch {
     $message = $e.Message
     Write-Error "Exception occured: $message; at line $line"
 }
-
-$siteUrl = $currentSite.Url
-$lockstate = $currentSite.LockState
-$title = $currentSite.Title
-
-# Property bag property listing
-<#
-$bag = Get-PnPPropertyBag -Connection $conn
-$lists = Get-PnPList -Connection $conn
-$appCatalog = ""
-
-foreach($property in $bag){
-    Write-Host "$($property.Key): $($property.Value)"
-}
-
-if($lists.Title -like "Apps for SharePoint"){
-    $appCatalog = "YES"
-}
-else{
-    $appCatalog = "NO"
-}
-#>
 
 $sqlAdmin = $settings.sqlAdmin
 $sqlPass = $settings.sqlPass
@@ -51,20 +28,72 @@ $srv = Get-SqlInstance -ServerInstance $settings.sqlUrl -Credential $cred
 $db = $srv.Databases[$settings.sqlDB]
 $table = $db.Tables[$settings.sqlDBTable] 
 
-$datatoDB = New-Object -TypeName psobject
+foreach($currentSite in $allSites){
+    
+    Connect-PnPOnline -Url $currentSite.Url -Connection $conn @connectionParameters
 
-$datatoDB | Add-Member -MemberType NoteProperty -Name "id" -Value ""
-$datatoDB | Add-Member -MemberType NoteProperty -Name "url" -Value $siteUrl
-$datatoDB | Add-Member -MemberType NoteProperty -Name "title" -Value $title
-$datatoDB | Add-Member -MemberType NoteProperty -Name "lockstate" -Value $lockstate
-$datatoDB | Add-Member -MemberType NoteProperty -Name "storage" -Value 5
+    $site = Get-PnPSite -Includes Usage
+    $lists = Get-PnPList
+    $storage = [math]::Round($site.Usage.Storage / 1000000,2)
+    $percentage = [math]::Round($site.Usage.StoragePercentageUsed,2)
+    
+    $appCatalog = ""
+    if($lists.Title -like "Apps for SharePoint"){
+        $appCatalog = "YES"
+    } else{
+        $appCatalog = "NO"
+    }
 
-try{
-    Write-SqlTableData -InputData @datatoDB -PassThru -InputObject $table
+    $scriptValue = ""
+    if($currentSite.DenyAddAndCustomizePages.value__ -eq 2){
+        $scriptValue = "FALSE"
+    } else {
+        $scriptValue = "TRUE"
+    }
+
+    $datatoDB = New-Object -TypeName psobject
+
+    $datatoDB | Add-Member -MemberType NoteProperty -Name "id" -Value ""
+    $datatoDB | Add-Member -MemberType NoteProperty -Name "url" -Value $currentSite.Url
+    $datatoDB | Add-Member -MemberType NoteProperty -Name "title" -Value $currentSite.Title
+    $datatoDB | Add-Member -MemberType NoteProperty -Name "lockstate" -Value $currentSite.LockState
+    $datatoDB | Add-Member -MemberType NoteProperty -Name "customScripts" -Value $scriptValue
+    $datatoDB | Add-Member -MemberType NoteProperty -Name "SPsiteTemplate" -Value $currentSite.Template
+    $datatoDB | Add-Member -MemberType NoteProperty -Name "SPSiteLocaleId" -Value $currentSite.LocaleId
+    $datatoDB | Add-Member -MemberType NoteProperty -Name "storagePercentage" -Value $percentage
+    $datatoDB | Add-Member -MemberType NoteProperty -Name "storageMB" -Value $storage
+    $datatoDB | Add-Member -MemberType NoteProperty -Name "appCatalog" -Value $appCatalog 
+
+
+    try{
+        Write-SqlTableData -InputData @datatoDB -PassThru -InputObject $table | Out-Null
+    }
+    catch {
+        $e = $_.Exception
+        $line = $_.InvocationInfo.ScriptLineNumber
+        $message = $e.Message
+        Write-Error "Exception occured: $message; at line $line"
+    }
+
+    Disconnect-PnPOnline
+    
 }
-catch {
-    $e = $_.Exception
-    $line = $_.InvocationInfo.ScriptLineNumber
-    $message = $e.Message
-    Write-Error "Exception occured: $message; at line $line"
+
+# Property bag property listing
+<#
+$bag = Get-PnPPropertyBag -Connection $conn
+ -Connection $conn
+$appCatalog = ""
+
+foreach($property in $bag){
+    Write-Host "$($property.Key): $($property.Value)"
 }
+#>
+
+
+
+
+
+
+
+#>
